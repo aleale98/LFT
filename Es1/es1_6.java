21fbfdaf5be536e0399696a49db3e99ec950b6e2
aleale98 <48025399+aleale98@users.noreply.github.com>
2020-01-31 12:01:55 +0100

    Update es1_6.java

--- Es1/es1_6.java
+++ Es1/es1_6.java
@@ -1,37 +1,33 @@
-//1.6
-public class E {
-    public static boolean scan(String s){
-        int state=0;
-        int i=0;
-        while(state>=0 && i<s.length()){
-            final char c=s.charAt(i++);
-            switch (state){
+public class es1_6 {
+    public static boolean scan(String s) {
+        int state = 0;
+        int i = 0;
+        while (state >= 0 && i < s.length()) {
+            final char c = s.charAt(i++);
+            switch (state) {
                 case 0:
-                    if(c=='0'){
+                    if(c=='0')
                         state=0;
-                    }else if(c=='1'){
+                    else if(c=='1')
                         state=1;
-                    }else{
+                    else
                         state=-1;
-                    }
                     break;
                 case 1:
-                    if(c=='0'){
+                    if(c=='0')
                         state=2;
-                    }else if(c=='1'){
+                    else if(c=='1')
                         state=0;
-                    }else{
+                    else
                         state=-1;
-                    }
                     break;
                 case 2:
-                    if(c=='0'){
+                    if(c=='0')
                         state=1;
-                    }else if(c=='1'){
-                        state=0;
-                    }else{
+                    else if(c=='1')
+                        state=2;
+                    else
                         state=-1;
-                    }
                     break;
             }
         }
