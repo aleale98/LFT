60617f2ee2c7473c79a62ef570bd2b52187fd451
aleale98 <48025399+aleale98@users.noreply.github.com>
2020-02-01 21:01:31 +0100

    Update Translator.java

--- Es5/Translator.java
+++ Es5/Translator.java
@@ -94,8 +94,9 @@ public class Translator {
                 lnext_true=code.newLabel();
                 lnext_false=code.newLabel();
                 match(Tag.WHILE);
-                bexpr(lnext_true, lnext_false);
                 code.emitLabel(lnext_true);
+				bexpr(lnext_true, lnext_false);
+				code.emitLabel(lnext_true);
                 stat();
                 code.emit(OpCode.GOto, btrue);
                 code.emitLabel(lnext_false);
@@ -111,18 +112,17 @@ public class Translator {
                 break;
             case '=':
                 match('=');
-                if (look.tag == Tag.ID) {
-                    int readAddress = st.lookupAddress(((Word) look).lexeme);
-                    if (readAddress == -1) {
-                        readAddress = count;
+                    read_id_addr = st.lookupAddress(((Word) look).lexeme);
+                    if (read_id_addr == -1) {
+                        read_id_addr = count;
                         st.insert(((Word) look).lexeme, count++);
-                    }
+                	}
                     match(Tag.ID);
                     expr();
-                    code.emit(OpCode.istore, readAddress);
+                    code.emit(OpCode.istore, read_id_addr);
 					break;
-					
-                }
+			default:
+			error("Syntax error in statp");
         }
     }
 
@@ -152,18 +152,17 @@ public class Translator {
 
     private void expr() {
         switch (look.tag) {
-            case Tag.NUM:
-			int val = Integer.parseInt(((NumberTok)look).lexeme);
-			int num_val=val;
-			match(Tag.NUM);
-            code.emit(OpCode.ldc, num_val);
-                break;
+			case Tag.NUM:
+        int num_val = Integer.parseInt(((NumberTok)look).lexeme);
+				match(Tag.NUM);
+        code.emit(OpCode.ldc, num_val);
+				break;
             case Tag.ID:
-                int readAddress = st.lookupAddress(((Word) look).lexeme);
-                if (readAddress == -1)
+                 int read_id_address = st.lookupAddress(((Word) look).lexeme);
+                if (read_id_address == -1)
                     error("var " + ((Word) look).lexeme + " has not been declared");
+				code.emit(OpCode.iload, read_id_address);
                 match(Tag.ID);
-                code.emit(OpCode.iload, readAddress);
                 break;
             case '(':
                 match('(');
@@ -236,7 +235,6 @@ public class Translator {
         switch (look.tag) {
             case '(':
                 match('(');
-                int next = code.newLabel();
                 bexprp(lnext_true, lnext_false);
                 match(')');
                 break;
@@ -245,59 +243,61 @@ public class Translator {
         }
     }
 
-    private void bexprp(int lnext_false, int lnext_true) {
-        OpCode opCode = OpCode.if_icmpeq;
-        switch (look.tag) {
-            case Tag.RELOP:
-                switch (((Word) look).lexeme) {
-                    case "<":
-                        opCode=OpCode.if_icmplt;
-                        break;
-                    case "<=":
-                        opCode=OpCode.if_icmple;
-                        break;
-                    case ">":
-                        opCode=OpCode.if_icmpgt;
-                        break;
-                    case ">=":
-                        opCode=OpCode.if_icmpge;
-                        break;
-                    case "<>":
-                        opCode=OpCode.if_icmpne;
-                        break;
-                    default:
-                        error(look.toString());
-                }
-                match(Tag.RELOP);
-                break;
+	private void bexprp(int lnext_true, int lnext_false) {
+    // inizializzazione a '==' se non serve e' perché c'è un errore
+    OpCode opCode = OpCode.if_icmpeq;
+		switch(look.tag){
+			case Tag.RELOP:
+				switch(((Word)look).lexeme){
+				  case "==":
+					opCode = OpCode.if_icmpeq;
+					break;
+				  case "<":
+					opCode = OpCode.if_icmplt;
+					break;
+				  case "<=":
+					opCode = OpCode.if_icmple;
+					break;
+				  case "<>":
+					opCode = OpCode.if_icmpne;
+					break;
+				  case ">":
+					opCode = OpCode.if_icmpgt;
+					break;
+				  case ">=":
+					opCode = OpCode.if_icmpge;
+					break;
+				  default:
+					error("Error in grammar (bexprp)");
+				}
+				match(Tag.RELOP);
+				expr();
+				expr();
+				code.emit(opCode, lnext_true);
+				code.emit(OpCode.GOto, lnext_false);
+				break;
 				case Tag.AND:
-					int and_true=code.newLabel();
+					int and_true = code.newLabel();
 					match(Tag.AND);
 					bexpr(and_true, lnext_false);
 					code.emitLabel(and_true);
 					bexpr(lnext_true, lnext_false);
-					code.emitLabel(lnext_false);
-				break;
-				case Tag.OR:
-				int or_false=code.newLabel();
-					match(Tag.OR);
-					bexpr(lnext_true, or_false);
-					code.emitlabel(or_false);
-					bexpr(lnext_true, lnext_false);
 					break;
-				case Tag.NEG:
-					match(Tag.NEG);
-					bexpr(lnext_false, lnext_true);
-					break;
+			case Tag.OR:
+				int or_false = code.newLabel();
+				match(Tag.OR);
+				bexpr(lnext_true, or_false);
+				code.emitLabel(or_false);
+				bexpr(lnext_true, lnext_false);
 				break;
-            default:
-                error(look.toString());
-        }
-        expr();
-        expr();
-        code.emit(opCode, lnext_true);
-        code.emit(OpCode.GOto, lnext_false);
-    }
+			case Tag.NOT:
+				match(Tag.NOT);
+				bexpr(lnext_false, lnext_true);
+				break;
+			default:
+				error("\nsyntax error in bexprp");
+		}
+	}
 
     private void elseopt(int lnext_false, int end) {
         switch (look.tag) {
